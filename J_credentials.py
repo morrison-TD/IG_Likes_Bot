b19e719aa14dcca18874a27b4ff7f9f38c5466ca
Tomeka <49292864+morrison-TD@users.noreply.github.com>
2020-11-16 19:39:35 -0600

    Update J_credentials.py

--- J_credentials.py
+++ J_credentials.py
@@ -1,5 +1,5 @@
 #Please update the username and password to reflect the IG account you'd like to log in as
 #"apple" "green"
 
-username = "urbanely.essential"
-pw = "Baylorgrad_14"
+username = "dog"
+pw = "cat"
